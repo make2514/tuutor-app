92e4342139c5b0209a8e97c2a024a3a10c4a8986
Markus Pikkanen <markus.pikkanen@gmail.com>
2018-09-28 19:22:23 +0300

    config eslint o client remove on server

--- server/app.js
+++ server/app.js
@@ -3,29 +3,29 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
-
-//Connect mongodb
+// Connect mongodb
 mongoose.connect('mongodb://localhost/myproject')
-    .then(console.log('connected'));
+  .then(() => console.log('connected'));
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error:'));
 db.once('open', () => {
-    console.log('Connection open to database');
+  console.log('Connection open to database');
 });
 
 const app = express();
 
-//Middleware
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-//Routing
+// Routing
 const usersRouter = require('./routes/userRoute');
+
 app.use('/users', usersRouter);
 
 module.exports = app;
