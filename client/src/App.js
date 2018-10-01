7d318e1efcdb435b9e90463e18e356a85eff9a7a
Nehat Sinani <nekhitys@gmail.com>
2018-10-01 12:25:35 +0300

    routers

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,18 @@ import Button from '@material-ui/core/Button';
 import logo from './logo.svg';
 import './App.css';
 import UsersList from "./components/usersList";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import LoginPage from "./components/loginPage";
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit
-                    {' '}
-                    <code>src/App.js</code>
-                    {' '}
-                    and save to reload.
-                </p>
-                <Button variant="contained" color="primary">
-                    Hello World
-                </Button>
-                <UsersList />
-            </div>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/login" exact component={LoginPage}/>
+                    <Route path="/" exact component={UsersList}/>
+                </Switch>
+            </BrowserRouter>
         );
     }
 }
