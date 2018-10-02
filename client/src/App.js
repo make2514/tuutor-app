c26b3bd3e52b73b7a1f09d84abb99d09b2e2ffd5
Toan Nguyen <toan.nguyen@frosmo.com>
2018-10-02 19:29:13 +0300

    Add ticket feed view

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,8 @@ import UsersList from "./components/usersList";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import LoginPage from "./components/loginPage";
 import SignIn from "./components/signIn";
-import Header from "./components/header/header";
-import Profile from "./components/profile/profilePage";
-import Avatar from "./components/profile/avatar";
+import TicketFeed from './components/ticketFeed/ticketFeed';
 import Register from "./components/registerPage";
-import CreateProfile from "./components/profile/createProfile";
 
 class App extends Component {
     render() {
@@ -16,11 +13,8 @@ class App extends Component {
                 <Switch>
                     <Route path="/login" exact component={LoginPage}/>
                     <Route path="/signin" exact component={SignIn}/>
+                    <Route path="/ticketfeed" exact component={TicketFeed}/>
                     <Route path="/" exact component={UsersList}/>
-                    <Route path="/header" exact component={Header}/>
-                    <Route path ="/profile" exact component={Profile}/>
-                    <Route path ="/createProfile" exact component={CreateProfile}/>
-                    <Route path ="/avatar" exact component={Avatar}/>
                     <Route path="/register" exact component={Register}/>
                 </Switch>
             </BrowserRouter>
