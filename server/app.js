1b50d33aca4887301c5e654823f7a0d3c1fa5459
Markus Pikkanen <markus.pikkanen@gmail.com>
2018-10-02 18:17:12 +0300

    basic user authentication

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+
 //Connection to mongodb
 require('./mongoose');
 
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const checkAuth = require('../middleware/checkAuth');
 
 const router = express.Router();
 
 /* This is an example and incomplete */
-router.get('/', (req, res, next) => {
+router.get('/', checkAuth, (req, res, next) => {
   userController.findAll()
       .then(obj => res.send(obj))
       //this error handling is not complete yet
@@ -21,14 +22,24 @@ router.post('/', (req, res, next) => {
 });
 
 router.post('/signup', (req, res, next) => {
-    userController.signUp({
-          username: req.body.username,
-          password: req.body.password
-      })
-        .then(createdUser => res.send(createdUser))
+    userController.signUp(req.body)
+        .then(createdUser => {
+            res.send(createdUser);
+        })
         .catch(err => {
             console.error(err);
-            res.staus(500).body(err);
+            res.status(500).json({ error: err });
+        });
+});
+
+router.post('/login', (req, res, next) => {
+    userController.login(req.body)
+        .then(response => {
+            res.send(response);
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: err });
         });
 });
 
