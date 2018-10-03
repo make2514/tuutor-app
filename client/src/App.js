289f93a2ed11846843148e43cf40a2e255f2e414
Toan Nguyen <toan.nguyen@frosmo.com>
2018-10-03 16:35:07 +0300

    Add myticket view prototype

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import UsersList from "./components/usersList";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import SignIn from "./components/signIn";
 import TicketFeed from './components/ticketFeed/ticketFeed';
+import MyTickets from './components/ticketFeed/myTickets';
 import Register from "./components/registerPage";
 import Header from "./components/header/header";
 import Profile from "./components/profile/profilePage";
@@ -18,7 +19,8 @@ class App extends Component {
                 <Switch>
                     <Route path="/signin" exact component={SignIn}/>
                     <Route path="/ticketfeed" exact component={TicketFeed}/>
-                    <Route path="/" exact component={UsersList}/>
+                    {/* <Route path="/" exact component={UsersList}/> */}
+                    <Route path="/mytickets" exact component={MyTickets}/>
                     <Route path="/register" exact component={Register}/>
                     <Route path="/header" exact component={Header}/>
                     <Route path ="/profile" exact component={Profile}/>
---
+++ client/src/components/ticketFeed/myTickets.js
@@ -0,0 +1,181 @@
+import React, {Component} from "react";
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import ImageIcon from '@material-ui/icons/Image';
+import WorkIcon from '@material-ui/icons/Work';
+import BeachAccessIcon from '@material-ui/icons/BeachAccess';
+import Header from '../header/header';
+import MyTicketItem from '../common/myTicketItem';
+
+function TabContainer(props) {
+    return (
+      <Typography component="div" style={{ padding: 8 * 3 }}>
+        {props.children}
+      </Typography>
+    );
+}
+
+TabContainer.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+        position: 'relative',
+        top: '56px'
+    },
+    selected: {
+        color: 'green',
+        background: 'red',
+    },
+});
+
+class TicketFeed extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+        value: 0,
+    };
+
+    handleChange = (event, value) => {
+        this.setState({ value });
+    };
+
+    renderTutorClasses() {
+        
+        return (
+            <TabContainer>
+                <List>
+                    <ListItem>
+                        <Avatar>
+                            <ImageIcon />
+                        </Avatar>
+                        
+                        <ListItemText style={{ zIndex: '1' }} primary={<MyTicketItem/>} secondary="Class description" />
+                    </ListItem>
+                    <ListItem>
+                        <Avatar>
+                            <ImageIcon />
+                        </Avatar>
+                        <ListItemText primary="Name" secondary="Class description" />
+                    </ListItem>
+                    <ListItem>
+                        <Avatar>
+                            <ImageIcon />
+                        </Avatar>
+                        <ListItemText primary="Name" secondary="Class description" />
+                    </ListItem>
+                    <ListItem>
+                        <Avatar>
+                            <ImageIcon />
+                        </Avatar>
+                        <ListItemText primary="Name" secondary="Class description" />
+                    </ListItem>
+                    <ListItem>
+                        <Avatar>
+                            <ImageIcon />
+                        </Avatar>
+                        <ListItemText primary="Name" secondary="Class description" />
+                    </ListItem>
+                    <ListItem>
+                        <Avatar>
+                            <ImageIcon />
+                        </Avatar>
+                        <ListItemText primary="Name" secondary="Class description" />
+                    </ListItem>
+                    <ListItem>
+                        <Avatar>
+                            <ImageIcon />
+                        </Avatar>
+                        <ListItemText primary="Name" secondary="Class description" />
+                    </ListItem>
+                    <ListItem>
+                        <Avatar>
+                            <ImageIcon />
+                        </Avatar>
+                        <ListItemText primary="Name" secondary="Class description" />
+                    </ListItem>
+                    <ListItem>
+                        <Avatar>
+                            <ImageIcon />
+                        </Avatar>
+                        <ListItemText primary="Name" secondary="Class description" />
+                    </ListItem>
+                    <ListItem>
+                        <Avatar>
+                            <ImageIcon />
+                        </Avatar>
+                        <ListItemText primary="Name" secondary="Class description" />
+                    </ListItem>
+                </List>
+            </TabContainer>
+        );
+    }
+
+    renderStudentRequests() {
+        return (
+            <TabContainer>
+                <List>
+                    <ListItem>
+                        <Avatar>
+                            <ImageIcon />
+                        </Avatar>
+                        <ListItemText primary="Name" secondary="Request description" />
+                    </ListItem>
+                    <ListItem>
+                        <Avatar>
+                            <ImageIcon />
+                        </Avatar>
+                        <ListItemText primary="Name" secondary="Request description" />
+                    </ListItem>
+                    <ListItem>
+                        <Avatar>
+                            <ImageIcon />
+                        </Avatar>
+                        <ListItemText primary="Name" secondary="Request description" />
+                    </ListItem>
+                </List>
+            </TabContainer>
+        );
+    }
+    
+    render() {
+        const { classes } = this.props;
+        const { value } = this.state;
+        return(
+            <div>
+                <Header />
+                <div className={classes.root}>
+                    <AppBar position="static">
+                    <Tabs centered value={value} onChange={this.handleChange}>
+                    <Tab label="Tutor classes" />
+                    <Tab label="Student requests" />
+                    </Tabs>
+                    </AppBar>
+                        {value === 0 &&
+                            this.renderTutorClasses()}
+                        {value === 1 &&
+                            this.renderStudentRequests()}
+                    </div>
+            </div>
+        )
+    }
+}
+
+TicketFeed.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(TicketFeed);
